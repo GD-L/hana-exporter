df27f05ca4edf203bed3d6893353315f7ecf294f
Greg <gregdelima@gmail.com>
2021-04-29 13:30:48 -0400

    Moved argparse to Main & cleaned up if/elif

--- pipeline.py
+++ pipeline.py
@@ -23,16 +23,9 @@ default_url = os.getenv("SERVER")
 default_port = os.getenv("PORT")
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-s", "--server" , help = "Server URL",default=default_url, dest = "server", type=str)
-parser.add_argument("-p", "--port" , help = "Port",default=default_port, dest = "port")
-parser.add_argument("-u", "--username", help = "Username", dest = "username", type=str)
-parser.add_argument("-P","--Password", help = "Password", dest = "password")
-args = parser.parse_args()
-
 def connection(server,port,username,password):
     conn = dbapi.connect(address = args.server, port = args.port, user=args.username, password = args.password)
-    print("\n Connected \n")
+    print("\n Connected  to " + args.server + " \n")
     return(conn)
 
 def execute_query(query,conn):
@@ -55,7 +48,7 @@ def read_input():
 
 
 
-def main():
+def main(args):
     """
     To automate this script then fill in the values for server, username, etc
     You will be prompted for any values set to ""
@@ -63,7 +56,7 @@ def main():
     Server and username can be entered on the command line as well.
 
     """
-
+    
     server = ""
     port = ""
     user_name = ""
@@ -71,36 +64,26 @@ def main():
     
     root = Tk()
     root.withdraw()
-    
-    if len(sys.argv)==4:
-        args.server = sys.argv[1]
-        args.port = sys.argv[2]
-        args.username = sys.argv[3]
-        args.password = sys.argv[4]
 
     print(args)
-    print("UN: " + user_name)
 
-    if server == "" and args.server == None:
-        args.server = input("Server:\n")
+    if server != "":
+        args.server = server
     
-    if port == "" and args.port == None:
-        args.port = input("Port: \n")
+    if port != "":
+        args.port = port
 
-    if user_name == "" and args.username == None:
-        args.username = input("Username:\n")
-    elif user_name != "" and args.username == None:
+    if user_name !="":
         args.username = user_name
-    else:
-        args.username
-    
-    if password == "" and args.password == None:
-        args.password = getpass("Password:\n")
-    elif password != "" and args.password == None:
-        args.password = password
-    else: 
-        args.password
+    elif args.username == None:
+        args.username = input("Enter Username: \n")
 
+    if password != "":
+        args.password = password
+    elif args.password == None:
+        args.password = getpass("Enter Password: \n")
+
+    print(args)
     conn = connection(args.server,args.port,args.username,args.password)
     source = input("Would you like to read from a file? (y/n) \n")
     if source.upper() == "Y":
@@ -117,4 +100,10 @@ def main():
         print(data)
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--server" , help = "Server URL",default=default_url, dest = "server", type=str)
+    parser.add_argument("-p", "--port" , help = "Port",default=default_port, dest = "port")
+    parser.add_argument("-u", "--username", help = "Username", dest = "username", type=str)
+    parser.add_argument("-P","--Password", help = "Password", dest = "password")
+    args = parser.parse_args()
+    main(args)
