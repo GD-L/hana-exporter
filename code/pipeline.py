e057dec32f5d6738209ae3d8c61e75af3a1c6ec1
Greg <gregdelima@gmail.com>
2021-10-27 12:13:15 -0400

    Linting Errors

--- code/pipeline.py
+++ code/pipeline.py
@@ -15,40 +15,43 @@ def connection(argv):
     """
     Defines the connection to an SAP HANA server.
     """
-    conn = dbapi.connect(address = argv.server,
-                        port = argv.port,
-                        user=argv.username,
-                        password = argv.password)
+    conn = dbapi.connect(address=argv.server,
+                         port=argv.port,
+                         user=argv.username,
+                         password=argv.password)
     print("\n Connected  to " + argv.server + " \n")
     return conn
 
-def execute_query(query,conn):
+
+def execute_query(query, conn):
     """
     Executes a SQL Query against the previously established connection.
     Uses pandas read_sql_query
     """
-    data = pd.read_sql_query(query,conn)
+    data = pd.read_sql_query(query, conn)
     return data
+
 
 def write_csv(data):
     """
     Writes a .CSV file using tkinter asksaveasfilename dialog
     """
     files = [("CSV", '*.csv'),
-            ("All Files","*.*")]
+             ("All Files", "*.*")]
     file_path = filedialog.asksaveasfilename(filetypes=files)
-    data.to_csv(file_path, index = False)
+    data.to_csv(file_path, index=False)
+
 
 def read_input():
     """
-    Uses tkinter to read a .txt .sql or other readable file containing a SQL query.
+    Uses tkinter to read a .txt .sql or
+    other readable file containing a SQL query.
     """
-    files = [("Text","*.txt"),("SQL","*.sql"),("All Files","*.*")]
-    file_path = filedialog.askopenfilename(filetypes = files)
-    read_file = open(file_path, "r", encoding = "UTF-8")
+    files = [("Text", "*.txt"), ("SQL", "*.sql"), ("All Files", "*.*")]
+    file_path = filedialog.askopenfilename(filetypes=files)
+    read_file = open(file_path, "r", encoding="UTF-8")
     sql_input = read_file.read()
     return sql_input, file_path
-
 
 
 def main(argv):
@@ -75,7 +78,7 @@ def main(argv):
     if port != "":
         argv.port = port
 
-    if user_name !="":
+    if user_name != "":
         argv.username = user_name
     elif argv.username is None:
         argv.username = input("Enter Username: \n")
@@ -91,7 +94,8 @@ def main(argv):
     if source.upper() == "Y":
         sql_query, file_path = read_input()
         print("Reading file "+str(file_path))
-    else: sql_query = input("Enter SQL Query:")
+    else:
+        sql_query = input("Enter SQL Query:")
 
     data = execute_query(sql_query, conn)
 
@@ -102,14 +106,24 @@ def main(argv):
     else:
         print(data)
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-s", "--server" ,
-                        help = "Server URL",
-                        default = default_url,
-                        dest = "server", type=str)
-    parser.add_argument("-p", "--port" , help = "Port", default = default_port, dest = "port")
-    parser.add_argument("-u", "--username", help = "Username", dest = "username", type=str)
-    parser.add_argument("-P","--Password", help = "Password", dest = "password")
+    parser.add_argument("-s", "--server",
+                        help="Server URL",
+                        default=default_url,
+                        dest="server",
+                        type=str)
+    parser.add_argument("-p", "--port",
+                        help="Port",
+                        default=default_port,
+                        dest="port")
+    parser.add_argument("-u", "--username",
+                        help="Username",
+                        dest="username",
+                        type=str)
+    parser.add_argument("-P", "--Password",
+                        help="Password",
+                        dest="password")
     args = parser.parse_args()
     main(args)
